1501b40038d4cc31fbc39a08f9fcc48202887e43
Sam Davyson <samdavyson@gmail.com>
2016-11-05 00:53:39 +0100

    Add saving from the URL
    Using the parameter /new/?url=
    
    e.g. /new/?url=davyson.com
    
    you can now save a link without ever seeing bookymarky

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,69 +3,68 @@ class LinksController < ApplicationController
 require 'nokogiri'
 require 'open-uri'
 require "pismo"
+require 'net/http'
 
   before_action :set_link, only: [:show, :edit, :update, :destroy]
- before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
   def index
-
     @user = current_user
 
-if @user != nil
-    @links = @user.links
-end
+    if @user != nil
+      @links = @user.links
+    end
 
     respond_with(@links)
   end
 
-
-
   def show
     @link = Link.find(params.fetch(:id))
-
     # @doc = Pismo::Document.new(@link.url, :reader => :cluster)
-
-
   end
 
-
-
-
   def new
-@link=current_user.links.build
-
-    #@link = Link.new
-    respond_with(@link)
+    @url = params.fetch(:url,'')
+    if @url != ''
+      if create_link(@url)
+        redirect_to LinksHelper.fix_url(@url)
+      end
+    else
+      @link=current_user.links.build
+      #@link = Link.new
+      respond_with(@link)
+    end
   end
 
   def edit
   end
 
-
-def create
-
-
-
-@url=params.fetch(:link).fetch(:url)
-
-doc = Pismo::Document.new(@url)
-
-
-doc2 = Nokogiri::HTML(open(@url))
-@content=doc2.to_html
-
-  @link = current_user.links.build(title: doc.title, url: @url,html:@content)
-    if @link.save
+  def create
+    @url=params.fetch(:link).fetch(:url)
+    if create_link(@url)
       redirect_to @link, notice: 'Link was saved'
     else
       render action: 'new'
     end
+  end
 
+  def create_link(url)
+    url=LinksHelper.fix_url(url)
+    #res=Net::HTTP.get_response(URI(url))
+    #url=res['location']
+    doc = Pismo::Document.new(url)
+    doc2 = Nokogiri::HTML(open(url))
+    content=doc2.to_html
 
+    @link = current_user.links.build(title: doc.title, url: url,html:content)
+    @link.save
+  end
 
-
+  def save
+    @url=params.fetch(:url)
+    redirect_to @url
   end
 
 
@@ -90,8 +89,7 @@ doc2 = Nokogiri::HTML(open(@url))
   end
 
   private
-
-def correct_user
+    def correct_user
       @link = current_user.links.find_by(id: params[:id])
       redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
     end
@@ -103,4 +101,4 @@ def correct_user
     def link_params
       params.require(:link).permit(:url,:title,:html)
     end
-end
+  end
--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,9 @@
 module LinksHelper
+
+	def self.fix_url(url)
+		if url[0...4] != 'http' && url[0...5] != 'https'
+		  url = 'http://' + url
+		end
+		return url
+	end
 end
